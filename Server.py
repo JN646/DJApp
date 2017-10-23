e1bb4d67e8055aefb26e94d74cb566538737afaf
JN646 <josh.ginn@hotmail.co.uk>
2017-10-23 19:29:57 +0100

    Removed text colours
    Removed the text colour class.

--- Server.py
+++ Server.py
@@ -7,19 +7,6 @@ from datetime import datetime
 from _thread import *
 from appJar import gui
 
-#Text Colours
-class colour:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
-
 #Declare
 host = '127.0.0.1'
 port = 5555
